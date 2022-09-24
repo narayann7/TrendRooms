fc985513beda0ea6db55a12c9901e4bf2fdb2bff
Narayan <laxminarayanreddy432@gmail.com>
2022-09-24 18:01:11 +0530

    initialize-app

---
+++ trend-rooms-client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      Happy coding!
+    </div>
+  );
+}
+
+export default App;
